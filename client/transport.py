d26702a6c639f7e4e6b9f62c66fced168ca0cf18
Nikhilesh A R <88420845+NikhileshAR@users.noreply.github.com>
2026-01-18 15:57:54 +0530

    Delete client/transport.py

--- client/transport.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def send(server, msg):
-    requests.post(f"{server}/send", json=msg)
-
-def poll(server, identity):
-    r = requests.get(f"{server}/poll", params={"identity": identity})
-    return r.json()["messages"]
